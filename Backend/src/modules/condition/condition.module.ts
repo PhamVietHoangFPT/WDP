a117dfbbfc766b259c4bf9b85b66fab46925dfec
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2025-05-22 12:19:46 +0700

    Format code

--- Backend/src/modules/condition/condition.module.ts
+++ Backend/src/modules/condition/condition.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { Condition, ConditionSchema } from './schemas/condition.schema'
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{ name: Condition.name, schema: ConditionSchema }]),
-    ],
-    exports: [MongooseModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Condition.name, schema: ConditionSchema },
+    ]),
+  ],
+  exports: [MongooseModule],
 })
-export class ConditionModule { }
+export class ConditionModule {}
--- Backend/src/modules/service/schemas/service.schema.ts
+++ Backend/src/modules/service/schemas/service.schema.ts
@@ -6,23 +6,31 @@ import { BaseEntity } from 'src/common/schema/baseEntity.schema'
 export type ServiceDocument = HydratedDocument<Service>
 @Schema()
 export class Service extends BaseEntity {
-    @Prop({
-        type: mongoose.Schema.Types.ObjectId,
-        default: () => new mongoose.Types.ObjectId(),
-    })
-    _id: mongoose.Schema.Types.ObjectId
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  })
+  _id: mongoose.Schema.Types.ObjectId
 
-    @Prop({ type: Number, required: true, min: 1 })
-    fee: number
+  @Prop({ type: Number, required: true, min: 1 })
+  fee: number
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Relationship', required: true })
-    relationshipId: mongoose.Schema.Types.ObjectId
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Relationship',
+    required: true,
+  })
+  relationshipId: mongoose.Schema.Types.ObjectId
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'TimeReturn', required: true })
-    timeReturnId: mongoose.Schema.Types.ObjectId
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'TimeReturn',
+    required: true,
+  })
+  timeReturnId: mongoose.Schema.Types.ObjectId
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Type', required: true })
-    typeId: mongoose.Schema.Types.ObjectId
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Type', required: true })
+  typeId: mongoose.Schema.Types.ObjectId
 }
 
 export const ServiceDocument = SchemaFactory.createForClass(Service)
