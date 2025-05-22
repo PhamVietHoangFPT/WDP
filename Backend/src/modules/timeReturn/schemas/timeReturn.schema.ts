3b9b6027f9106726627277136def1a6b761ceec5
SangDevK18 <sanghn1306@gmail.com>
2025-05-22 11:56:00 +0700

    create schema for Service ( everything related )

---
+++ Backend/src/modules/timeReturn/schemas/timeReturn.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+import mongoose from 'mongoose'
+import { BaseEntity } from 'src/common/schema/baseEntity.schema'
+
+export type TimeReturnDocument = HydratedDocument<TimeReturn>
+@Schema()
+export class TimeReturn extends BaseEntity {
+    @Prop({
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+    })
+    _id: mongoose.Schema.Types.ObjectId
+
+    @Prop({ type: Number, required: true })
+    timeReturn: number
+
+    @Prop({ type: Number, required: true })
+    timeReturnFee: number
+
+    @Prop({ type: String })
+    description: string
+
+}
+
+export const TimeReturnDocument = SchemaFactory.createForClass(TimeReturn)
