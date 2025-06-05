8b66753b3918a877d16eb2515efc29f38c26b893
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2025-06-05 21:51:18 +0700

    Update relationshipResponse.dto.ts

--- Backend/src/modules/relationship/dto/relationshipResponse.dto.ts
+++ Backend/src/modules/relationship/dto/relationshipResponse.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Exclude, Expose, Transform } from 'class-transformer'
 import mongoose from 'mongoose'
-import { Relationship } from '../schemas/relationship1.schema'
+import { Relationship } from '../schemas/relationship.schema'
 
 @Exclude()
 export class RelationshipResponseDto {
