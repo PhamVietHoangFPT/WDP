719d52c49fd98e403a9fb121d3e7eaf110d6df51
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2025-06-16 23:07:10 +0700

    service case history

---
+++ Backend/src/modules/testRequestHistory/interfaces/itestRequestHistory.repository.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import { TestRequestHistoryDocument } from '../schemas/testRequestHistory.schema'
+
+export interface ITestRequestHistoryRepository {
+  createTestRequestHistory(
+    serviceCaseId: string,
+    testRequestStatusId: string,
+    accountId: string,
+  ): Promise<TestRequestHistoryDocument>
+
+  findAllTestRequestHistory(
+    filter: Record<string, unknown>,
+  ): mongoose.Query<TestRequestHistoryDocument[], TestRequestHistoryDocument>
+
+  countDocuments(filter: Record<string, unknown>): Promise<number>
+}
+
+export const ITestRequestHistoryRepository = Symbol(
+  'ITestRequestHistoryRepository',
+)
