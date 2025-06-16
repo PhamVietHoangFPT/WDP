3c49281ddd2329a3c808abc67f2251917ec7fa79
SangDevK18 <sanghn1306@gmail.com>
2025-06-16 14:56:59 +0700

    craete type for service and related type

---
+++ WDP-FE/src/types/service.ts
@@ -0,0 +1,14 @@
+import type { Sample } from "./sample";
+import type { TimeReturn } from "./timeReturn";
+
+export interface Service {
+    id: string;
+    fee: number;
+    timeReturn: TimeReturn;
+    sample: Sample;
+    isAdministration: boolean;
+    isAgnate: boolean;
+    delete_by: string;
+    delete_at: Date;
+}
+
