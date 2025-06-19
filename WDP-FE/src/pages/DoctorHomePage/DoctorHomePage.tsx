ad5bf42c26d190818aff1ed3205655f6642011fb
Dien Nguyen Hoang <diennguyenhoang2902@gmail.com>
2025-06-19 10:05:33 +0700

    bac si tieng viet

--- WDP-FE/src/pages/DoctorHomePage/DoctorHomePage.tsx
+++ WDP-FE/src/pages/DoctorHomePage/DoctorHomePage.tsx
@@ -3,7 +3,7 @@ import 'dayjs/locale/vi' // Import ngôn ngữ tiếng Việt cho dayjs
 dayjs.locale('vi') // Thiết lập locale mặc định cho dayjs
 
 const DoctorHomePage = () => {
-  return <>Hello Doctor</>
+  return <>Xin chào bác sĩ</>
 }
 
 export default DoctorHomePage
