92e18c9db2d323dcc4d491dec5f700c7b2e7d905
SangDevK18 <sanghn1306@gmail.com>
2025-07-17 09:59:08 +0700

    finish module condition

---
+++ Backend/src/modules/condition/dto/createCondition.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+
+export class CreateConditionDto {
+    @ApiProperty({
+        example: 10000,
+        description: 'Phí của condition',
+    })
+    @IsNotEmpty()
+    @IsNumber()
+    conditionFee: number
+
+    @ApiProperty({
+        example: 'Tốt',
+        required: true,
+        type: String,
+    })
+    @IsString()
+    condition: string
+
+    constructor(partial: Partial<CreateConditionDto>) {
+        Object.assign(this, partial)
+    }
+}
