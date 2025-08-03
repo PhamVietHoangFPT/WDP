80236898a5335f2ebb2e7800616c89947966d6da
Dien Nguyen Hoang <diennguyenhoang2902@gmail.com>
2025-08-03 13:39:01 +0700

    thêm chức năng sort để filter cho bảng giá của customer

--- WDP-FE/src/features/customer/price.ts
+++ WDP-FE/src/features/customer/price.ts
@@ -2,13 +2,13 @@ import { apiSlice } from '../../apis/apiSlice'
 
 export const priceAPI = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    // ✅ Lấy danh sách ngân hàng VNPay
     getAllServiceCasePrice: builder.query({
-      query: () => ({
-        url: '/services',
+      query: (params) => ({
+        url: '/services', 
         method: 'GET',
+        params: params, 
       }),
-    }),   
+    }),
   }),
 })
 
